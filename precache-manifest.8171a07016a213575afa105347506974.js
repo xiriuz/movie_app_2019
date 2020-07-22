b7cb3db136c1f745733c6d73feca3a4b70bb9ac5
KursNim <kursnim@gmail.com>
2020-07-22 22:36:23 +0900

    Updates

---
+++ precache-manifest.8171a07016a213575afa105347506974.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53c3f77972e095147082cecc56f4ef5a",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "162fda07fa4fde034805",
+    "url": "/movie_app_2019/static/js/2.19a18da2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.19a18da2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d45159d7bbc8d34731e",
+    "url": "/movie_app_2019/static/js/main.3d45d192.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
